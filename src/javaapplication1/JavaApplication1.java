a75c55af6421d69bf11d48688c8b6ec61d5983cf
cstuser <cstuser>
2017-02-13 11:51:57 -0500

    added .gitignore and moved hello world in a method

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -2,8 +2,12 @@ package javaapplication1;
 
 public class JavaApplication1 {
 
-    public static void main(String[] args) {
+    public static void Hello()
+    {
         System.out.println("Hello World!!");
+    }
+    public static void main(String[] args) {
+        Hello();
     
     }
     
